1c4e4b5eb0bc410dd4ce25566ba300d846ec9357
nabeel4557 <143259191+nabeel4557@users.noreply.github.com>
2025-11-06 00:44:10 +0530

    Featured_section_completed

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { featuredProducts } from "@/data";
 
 function Featured() {
-    
+
     return (
         <div className="w-screen overflow-x-scroll text-red-500 bg-white">
             {/* wrapper */}
@@ -12,7 +12,7 @@ function Featured() {
                 {featuredProducts.map((item) => (
                     <div
                         key={item.id}
-                        className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300">
+                        className="w-screen h-[60vh] flex flex-col items-center justify-around p-4 hover:bg-fuchsia-50 transition-all duration-300 md:w-[50vw] xl:w-[33vw] xl:h-[90vh]">
                         {/* image cointainer */}
                         {item.img && (
                             <div className="relative flex-1 w-full hover:rotate-[60deg] transition-all duration-500">
@@ -25,11 +25,11 @@ function Featured() {
                             </div>
                         )}
                         {/* text container */}
-                        <div className="flex-1 flex flex-col items-center text-center gap-4">
-                            <h1 className="text-xl font-bold uppercase">
+                        <div className="flex-1 flex flex-col items-center justify-center text-center gap-4"> 
+                            <h1 className="text-xl font-bold uppercase xl:text-2xl 2xl:text-3xl">
                                 {item.title}
                             </h1>
-                            <p className="p-4">{item.desc}</p>
+                            <p className="p-4 2xl:p-8">{item.desc}</p>
                             <span className="text-xl font-bold">
                                 {item.price}
                             </span>
