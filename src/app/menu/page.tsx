897a42970492ba14649be1c890ca0a8dc10a6be0
muhammednabeel <nabeela@ribos.in>
2026-01-31 17:44:52 +0530

    Doing_MenuPage

--- src/app/menu/page.tsx
+++ src/app/menu/page.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { menu } from "../../data";
+import Link from "next/link";
 
 function MenuPage() {
-  return (
-    <div>MenuPage</div>
-  )
+    return (
+        <div className="p-4 lg:px-20 xl:px-40 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex flex-col md:flex-row items-center">
+            {menu.map((category) => (
+                <Link
+                    href={`/menu/${category.slug}`}
+                    key={category.id}
+                    className="w-full h-1/3 bg-right  p-8 md:h-1/2 "
+                    style={{ backgroundImage: `url(${category.img})` }}>
+                    <div className={`text-${category.color} w-1/2`}>
+                        <h1 className=" font-bold text-2xl">
+                            {category.title}
+                        </h1>
+                        <p className="text-sm my-8">{category.desc}</p>
+                        <button className={` hidden md:block bg-${category.color} text-${category.color === "black" ? "white" : "red-500"} text-red-500 py-2 px-4 rounded-md`}>Explore</button>
+                    </div>
+                </Link>
+            ))}
+        </div>
+    );
 }
 
-export default MenuPage
+export default MenuPage;
--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,23 +1,27 @@
 import Image from "next/image";
 import React from "react";
+import CountDown from "./CountDown";
 
 function Offer() {
     return (
-        <div className="bg-black h-screen flex flex-col md:flex-row">
+        <div className="bg-black h-screen flex flex-col md:flex-row md:justify-between md:bg-[url('/offerBg.png')] md:h-[70vh]">
             {/* text container  */}
             <div className=" flex-1 flex flex-col text-center items-center justify-center gap-8 p-6">
                 <h1 className=" text-white font-bold text-5xl md:text-6xl ">
                     Delicious Burger & French Fry
                 </h1>
-                <p className=" text-white">
+                <p className=" text-white xl:text-xl">
                     Progressively simplify effective e-toilers and
                     process-centric methods of empowerment. Quickly pontificate
                     parallel.
                 </p>
-                <button>Order Now</button>
+                <CountDown />
+                <button className=" bg-red-500 text-white py-3 px-6 rounded-md">
+                    Order Now
+                </button>
             </div>
             {/* image container */}
-            <div className=" relative flex-1 w-full">
+            <div className=" relative flex-1 w-full md:h-full">
                 <Image
                     src="/offerProduct.png"
                     alt=""
