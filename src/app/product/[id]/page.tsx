453c52d4b6a57983b0256c70cb4f403fbb50acf1
muhammednabeel <nabeela@ribos.in>
2026-02-05 17:35:39 +0530

    Single page completed

--- src/app/product/[id]/page.tsx
+++ src/app/product/[id]/page.tsx
@@ -5,10 +5,10 @@ import Price from "@/src/components/Price";
 
 function SingleProductPage() {
     return (
-        <div className="p-4 lg:p-20 xl:p-40 h-screen flex flex-col justify-around md:flex-row">
+        <div className="p-4 lg:p-20 xl:p-40 h-screen flex flex-col justify-around text-red-500 md:flex-row md:gap-8 md:items-center">
             {/* image container */}
             {singleProduct.img && (
-                <div className=" relative w-full h-1/2">
+                <div className=" relative w-full h-1/2 md:h-[60%]">
                     <Image
                         src={singleProduct.img}
                         alt=""
@@ -18,8 +18,10 @@ function SingleProductPage() {
                 </div>
             )}
             {/* text cointainer */}
-            <div className=" h-1/2">
-                <h1>{singleProduct.title}</h1>
+            <div className=" h-1/2 flex flex-col gap-4 md:h-[70%] md:justify-center md:gap-6 xl:gap-8">
+                <h1 className="text-3xl font-bold uppercase xl:text-4xl">
+                    {singleProduct.title}
+                </h1>
                 <p>{singleProduct.desc}</p>
                 <Price
                     price={singleProduct.price}
--- src/components/Price.tsx
+++ src/components/Price.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useEffect, useState } from "react";
 
 type Props = {
     price: number;
@@ -6,26 +7,48 @@ type Props = {
     options?: { title: string; additionalPrice: number }[];
 };
 function Price({ price, id, options }: Props) {
+    const [total,setTotal] = useState(price);
+    const [quantity,setQuantity] = useState(1);
+    const [selected, setSelected] = useState(0);
+
+    useEffect(() => {
+        setTotal(
+            quantity *   (options ? price + options[selected].additionalPrice:price)
+        )
+        
+        
+    },[quantity, selected, options, price])
     return (
-        <div>
-            <h2>${price.toFixed(2)}</h2>
+        <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-bold">${total.toFixed(2)}</h2>
             {/* options cointainer */}
-            <div className="">
-                {options?.map((option) => (
-                    <button key={option.title}>{option.title}</button>
+            <div className="flex gap-4">
+                {options?.map((option,index) => (
+                    <button
+                        key={option.title}
+                        className="min-w-[6rem] p-2 ring-1 ring-red-400 rounded-md" style={{
+                            background: selected === index ? "rgb(248 113 113)" : "white",
+                            color: selected === index ? "white": "red"
+                        }} onClick={()=>setSelected(index)}>
+                        {option.title}
+                    </button>
                 ))}
             </div>
             {/* quantity and add button cointainer */}
-            <div className="">
+            <div className="flex justify-between items-center">
                 {/* quantity */}
-                <div className="">
+                <div className=" flex justify-between w-full p-3 ring-1 ring-red-500">
                     <span>Quantity</span>
-                    <button>{"<"}</button>
-                    <span>1</span>
-                    <button>{">"}</button>
+                    <div className="flex gap-4 items-center">
+                        <button onClick={()=> setQuantity((prev)=> prev > 1 ? prev-1 : 1)}>{"<"}</button>
+                        <span>{quantity}</span>
+                        <button onClick={()=> setQuantity((prev)=> prev < 9 ? prev+1 : 9)}>{">"}</button>
+                    </div>
                 </div>
                 {/* cart button  */}
-                <button>Add to cart</button>
+                <button className=" uppercase bg-red-500 right-1 ring-red-500 p-3 text-white w-55">
+                    Add to cart
+                </button>
             </div>
         </div>
     );
