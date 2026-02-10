f748f927a04e71e0bce30d6c0e46a5eea276d5d8
muhammednabeel <nabeela@ribos.in>
2026-02-10 12:43:25 +0530

    doing CartPage

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
 function CartPage() {
-  return (
-    <div>CartPage</div>
-  )
+    return (
+        <div className="">
+            {/* product container */}
+            <div className="">
+                {/* single item  */}
+                <div className="">
+                    <Image
+                        src="/temporary/p1.png"
+                        alt=""
+                        width={100}
+                        height={100}
+                    />
+                    <div className="">
+                        <h1>sicilian</h1>
+                        <span>Large</span>
+                    </div>
+                    <h2>$79.90</h2>
+                    <span>X</span>
+                </div>
+            </div>
+            {/* payment container */}
+            <div className="h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)]">
+                <div className="">
+                    <span>Subtotal (3 items)</span>
+                    <span>$81.70</span>
+                </div>
+                <div className="">
+                    <span>Service Cost</span>
+                    <span>$0.00</span>
+                </div>
+                <div className="">
+                    <span className="">Delivery Cost</span>
+                    <span className="">FREE!</span>
+                </div>
+                <hr className="my-2" />
+                <div className="">
+                    <span className="">TOTAL(INCL. VAT)</span>
+                    <span className="">$81.70</span>
+                </div>
+                <button className="bg-red-500 text-white p-3 rounded-md w-1/2 ">
+                    CHECKOUT
+                </button>
+            </div>
+        </div>
+    );
 }
 
-export default CartPage
+export default CartPage;
